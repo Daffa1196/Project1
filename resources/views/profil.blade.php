97934987673914a897eca97a44e775fc898ee011
daffa <daffa@gmail.com>
2026-04-26 15:19:08 +0700

    halaman profil dan kontak selesai

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil</title>
+</head>
+<body>
+    <h1>Halaman Profil</h1>
+    <p>Ini halaman profil</p>
+
+    <a href="/kontak">Ke Halaman Kontak</a>
+</body>
+</html>
